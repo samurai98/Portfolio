7da3ea71a9d4ecf63e5b9db7716853b2007c935f
samurai98 <kirito1498@gmail.com>
2020-07-10 15:38:17 +0300

    refactor: fix specific project block

--- src/Projects/SpecificProject.js
+++ src/Projects/SpecificProject.js
@@ -10,11 +10,16 @@ function SpecificProject(props) {
     return (
         <Fade right cascade>
             <div className={styles.container}>
-                <div className={styles.backgroundImg} style={backgroundImg}>
-                    <a className={styles.btnLink} href={props.link} target="_blank">Смотреть</a>
+                <div className={styles.projectImg}>
+                    <div className={styles.backgroundImg} style={backgroundImg}></div>
+                    <a className={styles.btnLink} href={props.link} target="_blank">Tap to view</a>
                 </div>
+
                 <span className={styles.title}>{props.title}</span>
-                <span className={styles.description}>{props.description}</span>
+                <span className={styles.description}>
+                    {props.description}<br/><br/>
+                    <span>Tap on the image to view it!</span>
+                </span>
             </div>
         </Fade>
     );
